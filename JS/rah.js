9cd47ce37783c28ecc1b977ec9231df5a9084976
Rahul <Rahulcs2699@gmail.com>
2025-01-26 17:45:35 +0530

    Changes done

--- JS/rah.js
+++ JS/rah.js
@@ -1,17 +1,3 @@
-// var fActor="vijay"
-// var fPlayer="Dhoni"
-// var fMovie="kaththi"
-
-// function favourite()
-// {
-// console.log("favourite Actor:"+fActor)
-// console.log("favourite Player:"+fPlayer)
-// console.log("favourite movie:"+fMovie)
-// }
-
-// favourite() 
-
-
 function sumGreaterThanTarget(array, target) {
     return array
       .filter(element => element > target) // Filter elements greater than the target
@@ -22,6 +8,5 @@ function sumGreaterThanTarget(array, target) {
   const numbers = [10, 20, 5, 15, 30];
   const target = 15;
   const result = sumGreaterThanTarget(numbers, target);
-  console.log(result); // Output: 50 (20 + 30)  
-
-  sumGreaterThanTarget(array, target)
+  console.log(result); // Output: 50 (20 + 30)
+  
